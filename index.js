c329ef645c123ca8f2012d31efb870665eccde51
nischayshrestha <nikechrestha@gmail.com>
2025-01-06 14:43:51 +0545

    jsday1

---
+++ index.js
@@ -0,0 +1,18 @@
+const person= {
+    name: 'Nischay Shrestha',
+    id: 12,
+    salary: 5000,
+}
+console.log(person.name);
+const name= ['Nischay Shrestha','Nike Shrestha'];
+console.log(name);
+name.push('skd');//paxadi kunai kura add hunxa
+name.pop('skd');//paxadi kunai kura hatauxa
+console.log(name)
+console.log(name)
+name.shift();
+name.unshift('djidjoijdoiqd');
+console.log(name);
+const id= [12,13];
+console.log(id);
+9742835762
